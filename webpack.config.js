6cdd62452f92510d35a87f4b84175082cbcda9e3
王晓勇 <plortinus@gmail.com>
2017-03-01 15:43:12 +0800

    :heart: init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const config = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
+
+module.exports = config
